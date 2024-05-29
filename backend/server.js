be3324323a9414334a4df5553a5522e433aea315
vigneshxcode1 <vigneshxcode@gmail.com>
2024-05-29 17:36:50 +0530

    not found commit

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,11 @@ const __dirname = path.dirname(__filename);
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 // Catch all routes and return the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
 });
 
 
