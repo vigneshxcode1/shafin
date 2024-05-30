c81a9e5400f84fb5581a212ff2c06de9d5ea025d
vigneshxcode1 <vigneshxcode@gmail.com>
2024-05-30 13:15:05 +0530

    contact commit

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -10,10 +10,11 @@ const Contact = () => {
       <div className="contact-container">
         <div className="main-containers">
         <span>Contact Links</span>
+        <Link className="contact" to={"https://wa.me/9025630360"}>WhatsApp</Link>
+        <Link className="contact" to={"https://telegram.me/Deepakspax"}>Telegram</Link>
           <Link className="contact" href="https://www.instagram.com/ideepx_official/?hl=en">Instagram</Link>
-          <Link className="contact" to={"https://wa.me/9025630360"}>WhatsApp</Link>
           <Link className="contact" to={""}>Facebook</Link>
-          <Link className="contact" to={"https://telegram.me/Deepakspax"}>Telegram</Link>
+         
         </div>
       </div>
     </>
