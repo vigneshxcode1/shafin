310eebf6db4413c65f4a018c1acf190a9057bf6c
vigneshxcode1 <vigneshxcode@gmail.com>
2024-06-23 08:15:29 +0530

    zculter entry   commit

--- client/src/pages/Entry.jsx
+++ client/src/pages/Entry.jsx
@@ -2,10 +2,9 @@ import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Enter.css";
 import backgroundVideo from "../componets/images/entry.mp4"; 
-// import backgroundaudio from "../componets/images/background.mp3"
+
 const Entry = () => {
   const videoRef = useRef(null);
-  // const audioref=useRef(null)
 
   useEffect(() => {
     const video = videoRef.current;
@@ -16,58 +15,36 @@ const Entry = () => {
       });
     };
 
-    // const audio=audioref.current;
-    // const playaudio=()=>{
-    //   audio.play().catch((error) => {
-    //     console.log("Autoplay was prevented:", error);
-    //   });
-    // }
-
     playVideo();
-    // playaudio
 
     window.addEventListener('click', playVideo);
-// window.addEventListener('click',playaudio)
+
     return () => {
       window.removeEventListener('click', playVideo);
-      // window.removeEventListener('click', playaudio)
     };
   }, []);
 
   return (
     <div className="background">
-    <video ref={videoRef} src={backgroundVideo} type="video/mp4" autoPlay loop muted  playsInline  />
-   
-    {/* <audio ref={audioref} src={backgroundaudio}></audio> */}
-
-    <div className="text-overlay">
-      <h1 className="brand-img">Zculture</h1>
-      <nav className="main-links" id="main-link">
-        <Link to="/home">
-          <h1 className="link-item">SHOP</h1>
-        </Link>
-        <Link to="/postercollections">
-          <h1 className="link-item">POSTERS</h1>
-        </Link>
-        <Link to="/contact">
-          <h1 className="link-item">CONTACT</h1>
-        </Link>
-        <Link to="/cart">
-          <h1 className="link-item">CART</h1>
-        </Link>
-      </nav>
-      {/* <footer className="main-foot">
-        <a href="mailto:your@gmail.com" className="foot-item">
-          email
-        </a>
-        <Link className="foot-item" to="/sizechart">size chart</Link>
-        <p className="foot-item">shipping</p>
-        <Link className="foot-item" to="/about">about</Link>
-        <p className="foot-item">terms and conditions</p>
-      </footer> */}
+      <video ref={videoRef} src={backgroundVideo} type="video/mp4" autoPlay loop muted playsInline className="background-video" />
+      <div className="text-overlay">
+        <h1 className="brand-img">Zculture</h1>
+        <nav className="main-links" id="main-link">
+          <Link to="/home" className="link-main">
+            <h1 id="link"className="link-item">SHOP</h1>
+          </Link>
+          <Link to="/postercollections" className="link-main">
+            <h1 id="link" className="link-item">POSTERS</h1>
+          </Link>
+          <Link to="/contact" className="link-main">
+            <h1 id="link" className="link-item">CONTACT</h1>
+          </Link>
+          <Link to="/cart" className="link-main">
+            <h1 id="link" className="link-item">CART</h1>
+          </Link>
+        </nav>
+      </div>
     </div>
-  </div>
-  
   );
 };
 
